d7ca6e6ca089997d03d6c7deb26403906cf800a2
EdwinC <edwin.cruz@hotmail.com>
2024-05-19 23:30:13 -0600

    adding the first file and docker files

--- VersaSuit_1.0.0/Backend/User-Services/dist/index.js
+++ VersaSuit_1.0.0/Backend/User-Services/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("hellooo");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const appServer = (0, express_1.default)();
+appServer.set("port", 4000);
+appServer.use(express_1.default.json());
+appServer.listen(appServer.get("port"), () => {
+    console.log(`hello i am a Server on port ${appServer.get("port")}`);
+});
--- VersaSuit_1.0.0/Backend/User-Services/src/index.ts
+++ VersaSuit_1.0.0/Backend/User-Services/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hellooo")
+import express from "express";
+
+const appServer = express();
+
+appServer.set("port", 4000);
+appServer.use(express.json());
+
+appServer.listen(appServer.get("port"), () => {
+    console.log(`hello i am a Server on port ${appServer.get("port")}`);
+});
